7cd5c40096d3de6181143e70db8802165e8f8395
seaeye <1261682886@qq.com>
2013-10-16 21:22:35 +0800

    add class1 test
    you see

---
+++ 1.2_Service/Service.Code/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero.Service.Code
+{
+    public class Class1
+    {
+        /// <summary>
+        /// 对象ID
+        /// </summary>
+        public string ObjectID { get; set; }
+    }
+}
